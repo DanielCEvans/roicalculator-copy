39477131fb07468b6f812ebb1466711b1ca392ca
DanielCEvans <danielconnorevans@gmail.com>
2023-04-22 16:06:17 +1000

    zustand implementation message

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// This branch will be used to implement zustand state library
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
